639a074721e37e433a64b7e4ef2aabc44fe6babc
Borjand <bdorado@pa.uc3m.es>
2025-06-25 16:39:13 +0200

    added auxilar script for configuration

---
+++ assets/utils/add-static-routes.sh
@@ -0,0 +1,26 @@
+# Script to add static routes on the pods after macsec configuration
+#!/bin/bash
+
+NAMESPACE="8e05312c-c696-4fe9-9df9-e389ff126dde"
+CONTAINER="sec-agent" 
+ROUTE_COMMAND="ip route add 10.98.0.0/24 via 10.78.0.10"
+
+PODS=(
+  haproxy-lb
+  web-server-0
+  web-server-1
+  web-server-2
+)
+
+# Routes through router
+for POD in "${PODS[@]}"; do
+  echo "➤ Añadiendo ruta en pod: $POD"
+  kubectl exec -n "$NAMESPACE" "$POD" -c "$CONTAINER" -- bash -c "$ROUTE_COMMAND"
+done
+
+# Static route for router through the GRE tunnel
+POD=router
+ROUTE_COMMAND="ip route add 10.98.0.0/24 via 10.88.0.12"
+echo "➤ Añadiendo ruta en pod: $POD"
+kubectl exec -n "$NAMESPACE" "$POD" -c "$CONTAINER" -- bash -c "$ROUTE_COMMAND"
+
